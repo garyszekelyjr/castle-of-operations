0234ecef1ec18ee09881b98b3a8c7eafa2df72b2
Gary Szekely <szekely.gary2000@gmail.com>
2022-12-03 19:57:23 -0500

    third person controller and some setup for tutorial

---
+++ Assets/Scripts/TutorialLevel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialLevel : MonoBehaviour
+{
+    public GameObject username_text;
+    public GameObject health_bar;
+    public GameObject stamina_bar;
+    public GameObject compass;
+    public GameObject level_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
